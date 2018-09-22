6939892079fde41e8a5dabc55c8dfab32459a389
Julia Komeristaya <juliyakomeristaya@gmail.com>
2018-09-22 16:35:05 +0300

    add task 2

---
+++ src/test/java/home1/HomeWork2.java
@@ -0,0 +1,24 @@
+package home1;
+
+
+
+import java.util.Scanner;
+
+public class HomeWork2 {
+
+    public static void decomposeN(int number) {
+        String num = Integer.toString(number);
+        int i = 4;
+        int length = Integer.toString(number).length();
+        while (i < length & i >=0) {
+            char result = num.charAt(i--);
+            System.out.println(result);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter a 5-digit number:" );
+        int number = console.nextInt();
+        decomposeN(number);
+    }
+}
